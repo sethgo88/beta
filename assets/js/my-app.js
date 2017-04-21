990f8555c5845975707d9909fc9f1790ee368f3e
sethgo88 <seth.oharra@gmail.com>
2017-04-21 14:11:07 -0700

    first build

---
+++ assets/js/my-app.js
@@ -0,0 +1,10 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    domCache: true
+});
